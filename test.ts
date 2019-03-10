0bc7f7d7a58b733a1fb94e0de943657e20bb56d6
Gurpreet singh <amansingh9569@gmail.com>
2019-03-10 15:15:36 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,6 @@
 import {solve} from './linalg';
 import * as tf from '@tensorflow/tfjs';
-let a = tf.tensor([[1,1,2],[0.5,5,0],[0,1,6]]);
-let b = tf.tensor([[1,3],[4,1],[1,1]]);
+let a = tf.tensor([[1,1,-2],[1,2,1],[-2,1,1]]);
+let b = tf.tensor([[1],[1],[1]]);
 let x = solve(a, b);
 x.print();
-
